563a807efe7805d874f2a79f0f7484445cd8dfaf
Ron <ronisr@post.bgu.ac.il>
2021-04-03 15:35:14 +0300

    Ron

--- AES/src/Main.java
+++ AES/src/Main.java
@@ -1,35 +1,39 @@
 import java.io.*;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
 
 public class Main {
-	private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+//	private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
 	public static void main(String[] args) {
 
-		print(toHexString("./files/message_short"));
-
-
+		String[][] s = toHexString("./files/message_short");
+		print2DArr(s);
+		
+		
 	}
 
-	public static List<String> toHexString(String path) {
-		List<String> sb = new LinkedList<>();
+	public static String[][] toHexString(String path) {
+		String[][] s = new String[4][4];
 		File file = new File(path);
+		int i=0,j=0;
 		byte[] bytes;
 		try {
 			bytes = Files.readAllBytes(file.toPath());
 			if (bytes != null) 
 				for (byte b:bytes) {
+					if (i==4) {
+						i=0;
+						j++;
+					}
 					final String hexString = Integer.toHexString(b & 0xff);
 					if(hexString.length()==1)
-						sb.add("0");
-					sb.add(hexString);
+						s[i][j] = "0";
+					s[i][j] = hexString;
+					i++;
 				}
 		} catch (IOException e) {}
-		return sb;
+		return s;
 	}
 
 	public static void print(Object o) {
@@ -39,6 +43,16 @@ public class Main {
 	public static void printArr(char[] arr) {
 		for(Object o:arr)
 			System.out.print(o+" ");
+	}
+	
+	public static void print2DArr(String[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j]);
+				if(j<4) System.out.print(" ");
+			}
+			System.out.println();
+		}
 	}
 
 
